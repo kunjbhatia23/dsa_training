79a866bb71efa0f8b513e1c6566c43f0423a9921
Kunj Bhatia <kunjbhatia23@gmail.com>
2025-01-14 23:25:04 +0530

    Add files via upload

---
+++ powerofno.java
@@ -0,0 +1,12 @@
+public class powerofno {
+    private static int power (int n, int p){
+        if(p==0){
+            return 1;
+        }
+        return power(n, p-1)*n;
+        }
+    public static void main(String[] args) {
+        int result = power(2,5);
+        System.out.println(result);
+    }
+}
